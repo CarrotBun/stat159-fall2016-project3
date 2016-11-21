8eaa445405e63c46ce95d253fd4bad2945d53389
Bryana Gutierrez <bygutierrez@berkeley.edu>
2016-11-21 00:13:18 -0800

    script: adding thr script that will create the training and testing data sets once the scaled data is available

---
+++ code/scripts/train-test-sets-script.R
@@ -0,0 +1,19 @@
+#This code will parse out what data is used for the train or tests sets. 
+#The train set will be used to build the model, and the test will be used to test the model. 
+
+#Loading in the data to be parsed. 
+scaled_colleges <- read.csv('data/name of scaled colleges.csv', header = TRUE)
+scaled_colleges$X <- NULL
+
+#creating a sample of indices 
+set.seed(12345)
+indx <- sample(1:nrow(scaled_colleges), .75*nrow(scaled_colleges))
+
+#creating a train set using the indices chosen from the sample above. 
+train_set <- scaled_colleges[indx, ]
+
+#creating a test set from the indices not chosen from the sample above. 
+test_set <- scaled_colleges[-indx, ]
+
+#Saving the data sets.
+save(train_set, test_set, file = "data/RData-files/train-test-sets.RData")
