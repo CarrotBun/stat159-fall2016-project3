4175a33cc72e7df5caa22359dd24127e2ee260ef
Elly Wang <ellywjy@berkeley.edu>
2016-12-05 21:28:40 -0800

    cleaning: added comments to code

--- code/scripts/cleaning.R
+++ code/scripts/cleaning.R
@@ -1,8 +1,14 @@
 #setwd("~/stat159/project3/")
+# Set Directory
 setwd("data/raw/")
+
+# Load necessary library
 library(dplyr)
+
+# Load functions
 source("../../code/functions/cleaning-helpers.R")
 
+# Read raw files for 2008-2014
 data2008 <- read.csv("MERGED2008_09_PP.csv")
 data2009 <- read.csv("MERGED2009_10_PP.csv")
 data2010 <- read.csv("MERGED2010_11_PP.csv")
@@ -12,9 +18,7 @@ data2013 <- read.csv("MERGED2013_14_PP.csv")
 data2014 <- read.csv("MERGED2014_15_PP.csv")
 
 
-test <- select_data(data2010)
-## Only 2 years have WN_EARN_WNE_P10
-
+# Select only necessary columns from original data
 data2008 <- select_data(data2008)
 data2009 <- select_data(data2009)
 data2010 <- select_data(data2010)
@@ -23,8 +27,10 @@ data2012 <- select_data(data2012)
 data2013 <- select_data(data2013)
 data2014 <- select_data(data2014)
 
+# Locale and CCUGPROF is only available for 2014, extra info to apply to other years
 locale_category <- select(data2014, OPEID, LOCALE, CCUGPROF)
 
+# Apply LOCALE and CCUGPROF info to all other years
 data2008 <- apply_info(data2008, locale_category)
 data2009 <- apply_info(data2009, locale_category)
 data2010 <- apply_info(data2010, locale_category)
@@ -32,7 +38,7 @@ data2011 <- apply_info(data2011, locale_category)
 data2012 <- apply_info(data2012, locale_category)
 data2013 <- apply_info(data2013, locale_category)
 
-
+# Add year information before merge
 data2008$Year = "2008"
 data2009$Year = "2009"
 data2010$Year = "2010"
@@ -41,9 +47,12 @@ data2012$Year = "2012"
 data2013$Year = "2013"
 data2014$Year = "2014"
 
+# combine all years' data together
 data <- rbind(data2008, data2009, data2010, data2011, data2012, data2013, data2014)
+# make sure certain columns are factored
 data <- factor_this(data)
 
+# saving data in both RData and csv files
 save(data, file = "../RData-files/colleges.RData" )
 write.csv(data, file = "../datasets/colleges.csv")
 
